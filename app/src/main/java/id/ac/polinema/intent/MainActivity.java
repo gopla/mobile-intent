20c16aff92997cdf54ed6d3167d2a9ffb84c80d6
Naufal Yudhistira <yudhistn@gmail.com>
2020-02-17 14:34:46 +0700

    Add Explicit Intent function

--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -1,6 +1,9 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,4 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void handleExplicitIntent(View view) {
+        startActivity(new Intent(this, ExplicitIntentActivity.class));
+    }
+
+    public void handleExit(View view) {
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
+    }
 }
