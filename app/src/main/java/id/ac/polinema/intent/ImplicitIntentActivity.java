2b0341b2d994c5cac3a284a28cdc2ea77a4a4d8d
Naufal Yudhistira <yudhistn@gmail.com>
2020-02-20 20:44:14 +0700

    Add camera

--- app/src/main/java/id/ac/polinema/intent/ImplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ImplicitIntentActivity.java
@@ -1,7 +1,12 @@
 package id.ac.polinema.intent;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -49,9 +54,61 @@ public class ImplicitIntentActivity extends AppCompatActivity {
                 }
             }
         }
+
+        if(resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        avatarImage.setImageBitmap(selectedImage);
+                    }
+
+                    break;
+                case 1:
+                    if (data != null){
+                        try {
+                            Uri imageUri = data.getData();
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                            avatarImage.setImageBitmap(bitmap);
+                        }catch (IOException e){
+                            Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                            Log.e(TAG, e.getMessage());
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void selectImage(Context context) {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose your profile picture");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo")) {
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto , 1);
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
     }
 
     public void handleChangeAvatar(View view) {
-        startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), GALLERY_REQUEST_CODE);
+        selectImage(this);
+//        startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), GALLERY_REQUEST_CODE);
     }
 }
